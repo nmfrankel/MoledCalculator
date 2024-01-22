da8f43747e1a42780b08c98724f57fe5690387b0
Nosson M Frankel <nossonmfrankel@gmail.com>
2024-01-21 20:59:23 -0500

    Cleaned code & doesn't seem to work for unknown reason

--- calculate.py
+++ calculate.py
@@ -54,7 +54,7 @@ START_DATE = {
 	'month': 0,
 	'days': 0,
 	'day_of_week': 3,
-	'hours': 6,
+	'hours': 18,
 	'minutes': 0,
 	'cholokim': 0
 }
@@ -82,12 +82,12 @@ LAST_MOLAD = {
 # Shabbos ﴾Oct 14﴿ 6:33 PM + 1 מולד חודש חשון: חלק
 # Monday ﴾Nov 13﴿ 7:17 AM + 2 מולד חודש כסלו: חלקים
 # Tuesday ﴾Dec 12﴿ 8:01 PM + 3 מולד חודש טבת: חלקי
-# Thursday ﴾Jan 11﴿ 8:45 AM + 4 מולד חודש שבט: חלקים
+# Thursday ﴾Jan 11﴿ 8:45 AM + 4 מולד חודש שבט: חלקים #####
 # Friday (Feb 9) 9:29 PM + 5 מולד חודש אדר‐א: חלקים
 # Sunday (Mar 10) 10:13 AM + 6 מולד חודש אדר‐ב: חלקים
 # Monday ﴾Apr 8﴿ 10:57 PM + 7 מולד חודש ניסן: חלקים
 # Wednesday ﴾May 8﴿ 11:41 AM + 8 מולד חודש אייר: חלקי
-# Friday ﴾Jun 7﴿ 12:25 AM + 9 מולד חודש סיון: חלקי #####
+# Friday ﴾Jun 7﴿ 12:25 AM + 9 מולד חודש סיון: חלקי
 # Shabbos ﴾Jul 6﴿ 1:09 PM + 10 מולד חודש תמוז: חלקי
 # Monday ﴾Aug 5﴿ 1:53 AM + 11 מולד חודש אב: חלקים
 # Tuesday ﴾Sep 3﴿ 2:37 PM + 12 מולד חודש אלול: חלקי
@@ -99,18 +99,18 @@ def print_dictionary (dictionary):
 		print(f"{key}: {value}")
 
 
-""" Format to readable timestamp """
+def check_lunar_leap_year(hebrew_year):
+    year_in_cycle = hebrew_year % 19
+    return year_in_cycle in [3, 6, 8, 11, 14, 17, 19]
+
+
+""" Format timestamp to readable string """
 def readable_molad (year, month, days, day_of_week, hours, minutes, cholokim, *rest):
 	month_name = HEBREW_MONTHS[month] if not (check_lunar_leap_year(year) and month == 11) else 'Adar I'
 	day_of_week_name = DAYS_OF_WEEK[day_of_week]
 	time = "{:01}:{:02} {}".format(hours % 12 or 12, minutes, "AM" if hours < 12 else "PM")
 
 	return f"[Molad {month_name} {year}] {day_of_week_name} {time} + {cholokim} cholokim"
-
-
-def check_lunar_leap_year(hebrew_year):
-    year_in_cycle = hebrew_year % 19
-    return year_in_cycle in [3, 6, 8, 11, 14, 17, 19]
 
 
 """ For a given molad, calculate the next molad """
@@ -131,12 +131,11 @@ def calculate_molad(year, month, days, day_of_week, hours, minutes, cholokim):
 	molad_day_of_week = (new_day_of_week + math.floor((new_hour + 1) / LUNAR_CYCLE_TERMS['hours'])) % LUNAR_CYCLE_TERMS['days_in_week']
 	molad_hour = (new_hour + math.floor(new_minute / LUNAR_CYCLE_TERMS['minutes'])) % LUNAR_CYCLE_TERMS['hours']
 	molad_minute = (new_minute + math.floor(new_cholokim / LUNAR_CYCLE_TERMS['cholokim'])) % LUNAR_CYCLE_TERMS['minutes']
-	molad_cholokim = cholokim + LUNAR_MONTH['cholokim'] % LUNAR_CYCLE_TERMS['cholokim']
+	molad_cholokim = (cholokim + LUNAR_MONTH['cholokim']) % LUNAR_CYCLE_TERMS['cholokim']
 
 	return {
 		'year': molad_year,
         'month': molad_month,
-		# 'month_name': molad_month_name,
 		'days': 0,
         'day_of_week': molad_day_of_week,
         'hours': molad_hour,
@@ -149,7 +148,7 @@ def main():
 	past_molad = next_molad = LAST_MOLAD
 	print(readable_molad(**past_molad))
 
-	while not (past_molad['year'] > END_DATE['year'] or (past_molad['month'] == END_DATE['month'] and past_molad['year'] == END_DATE['year'])):
+	while not (past_molad['month'] == END_DATE['month'] and past_molad['year'] >= END_DATE['year']):
 		next_molad = calculate_molad(**past_molad)
 		print(readable_molad(**next_molad))
 		past_molad = next_molad
